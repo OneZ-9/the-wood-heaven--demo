0513f9031c9ba1b1a064ea5069f7b0709b26cc0f
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-05-06 20:26:48 +0530

    Build AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
@@ -17,14 +18,17 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />{" "}
-          {/* Redirect to dashboard in path '/' */}
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />{" "}
+            {/* Redirect to dashboard in path '/' */}
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>

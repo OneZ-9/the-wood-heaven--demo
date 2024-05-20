3f764e6e416eb5e62ad70abc879ba6e2e6998fd9
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-05-20 13:22:11 +0530

    Implemented client side sorting cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -41,6 +41,7 @@ function CabinTable() {
 
   if (!cabins.length) return <Empty resourceName="cabins" />;
 
+  // 1. Filter
   const filterValue = searchParams.get("discount") || "all";
   // console.log(filterValue);
 
@@ -50,6 +51,14 @@ function CabinTable() {
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // 2. Sort
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-"); // destructuring the returned array from split()
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -65,7 +74,8 @@ function CabinTable() {
 
         <Table.Body
           // data={cabins}
-          data={filteredCabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
 

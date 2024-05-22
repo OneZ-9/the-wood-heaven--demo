b226e344ae3d4aa1cee8758a82afadf1fc55dece
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-05-22 21:56:22 +0530

    Implement separate Price component to fix console error

--- src/features/bookings/BookingDataBox.jsx
+++ src/features/bookings/BookingDataBox.jsx
@@ -11,6 +11,7 @@ import DataItem from "../../ui/DataItem";
 import { Flag } from "../../ui/Flag";
 
 import { formatDistanceFromNow, formatCurrency } from "../../utils/helpers";
+import Price from "../../ui/Price";
 
 const StyledBookingDataBox = styled.section`
   /* Box */
@@ -68,31 +69,31 @@ const Guest = styled.div`
   }
 `;
 
-const Price = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1.6rem 3.2rem;
-  border-radius: var(--border-radius-sm);
-  margin-top: 2.4rem;
+// const Price = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   padding: 1.6rem 3.2rem;
+//   border-radius: var(--border-radius-sm);
+//   margin-top: 2.4rem;
 
-  background-color: ${(props) =>
-    props.isPaid ? "var(--color-green-100)" : "var(--color-yellow-100)"};
-  color: ${(props) =>
-    props.isPaid ? "var(--color-green-700)" : "var(--color-yellow-700)"};
+//   background-color: ${(props) =>
+//     props.isPaid ? "var(--color-green-100)" : "var(--color-yellow-100)"};
+//   color: ${(props) =>
+//     props.isPaid ? "var(--color-green-700)" : "var(--color-yellow-700)"};
 
-  & p:last-child {
-    text-transform: uppercase;
-    font-size: 1.4rem;
-    font-weight: 600;
-  }
+//   & p:last-child {
+//     text-transform: uppercase;
+//     font-size: 1.4rem;
+//     font-weight: 600;
+//   }
 
-  svg {
-    height: 2.4rem;
-    width: 2.4rem;
-    color: currentColor !important;
-  }
-`;
+//   svg {
+//     height: 2.4rem;
+//     width: 2.4rem;
+//     color: currentColor !important;
+//   }
+// `;
 
 const Footer = styled.footer`
   padding: 1.6rem 4rem;
---
+++ src/ui/Price.jsx
@@ -0,0 +1,37 @@
+import styled, { StyleSheetManager } from "styled-components";
+
+const StyledPrice = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1.6rem 3.2rem;
+  border-radius: var(--border-radius-sm);
+  margin-top: 2.4rem;
+
+  background-color: ${(props) =>
+    props.isPaid ? "var(--color-green-100)" : "var(--color-yellow-100)"};
+  color: ${(props) =>
+    props.isPaid ? "var(--color-green-700)" : "var(--color-yellow-700)"};
+
+  & p:last-child {
+    text-transform: uppercase;
+    font-size: 1.4rem;
+    font-weight: 600;
+  }
+
+  svg {
+    height: 2.4rem;
+    width: 2.4rem;
+    color: currentColor !important;
+  }
+`;
+
+function Price({ children, isPaid }) {
+  return (
+    <StyleSheetManager shouldForwardProp={(prop) => prop !== "isPaid"}>
+      <StyledPrice isPaid={isPaid}>{children}</StyledPrice>
+    </StyleSheetManager>
+  );
+}
+
+export default Price;

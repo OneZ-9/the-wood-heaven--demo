6021317e0d64e6cc1fce6eda330318ff508d3b90
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-05-30 00:02:28 +0530

    Added stats into dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +12,35 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading: isLoadingRecentBookings, recentBookings } =
+    useRecentBookings();
+  const {
+    isLoading: isLoadingRecentStays,
+    confirmedStays,
+    recentStays,
+    numDays,
+  } = useRecentStays();
+  // console.log(recentBookings);
+  const { cabins, isLoading: isLoadingCabins } = useCabins();
+
+  if (isLoadingRecentBookings || isLoadingRecentStays || isLoadingCabins)
+    return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        recentBookings={recentBookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Today's activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
